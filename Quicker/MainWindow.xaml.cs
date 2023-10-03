4b657f825718349b5c080468913b7750bcb5cbb7
rta_28 <42664466+rtasan@users.noreply.github.com>
2023-10-03 12:04:30 +0900

    change

--- Quicker/MainWindow.xaml.cs
+++ Quicker/MainWindow.xaml.cs
@@ -26,8 +26,49 @@ namespace Quicker
         public MainWindow()
         {
             InitializeComponent();
+            Task.Run(() => SetKey());
+            System.Diagnostics.Debug.WriteLine("Im not stopped");    
         }
 
-       
+        public void SetKey()
+        {
+            using (var Keyboard = WindowsInput.Capture.Global.KeyboardAsync())
+            {
+
+                var Listener = new WindowsInput.Events.Sources.TextSequenceEventSource(Keyboard, new WindowsInput.Events.TextClick("aaa"));
+                Listener.Triggered += (x, y) => Listener_Triggered(Keyboard, x, y); ;
+                Listener.Enabled = true;
+                while (true)
+                {
+
+                }
+            }
+            
+
+        }
+
+        
+
+        private static async void Listener_Triggered(IKeyboardEventSource Keyboard, object sender, WindowsInput.Events.Sources.TextSequenceEventArgs e)
+        {
+            e.Input.Next_Hook_Enabled = false;
+
+            var ToSend = WindowsInput.Simulate.Events();
+            for (int i = 1; i < e.Sequence.Text.Length; i++)
+            {
+                ToSend.Click(WindowsInput.Events.KeyCode.Backspace);
+            }
+
+            ToSend.Click("Always ask albert!");
+
+            //We suspend keyboard events because we don't want to accidently trigger a recursive loop if our
+            //sending text actually had 'aaa' in it.
+            using (Keyboard.Suspend())
+            {
+                await ToSend.Invoke();
+            }
+        }
     }
+    
+    
 }
