b229bfea3c9132843eefede6f335ee35f9a35f10
rta_28 <rtasansax@gmail.com>
2023-10-07 17:06:57 +0900

    added OnlyNumber

--- Quicker/Match.cs
+++ Quicker/Match.cs
@@ -42,5 +42,22 @@ namespace Quicker
                 ToSend.Invoke();
             }
         }
+        public void OnlyNumber(ref IKeyboardEventSource Keyboard, string? OptionalText, string number)
+        {
+            var ToSend = WindowsInput.Simulate.Events();
+            //System.Diagnostics.Debug.WriteLine(this.keyword.Length + (String.IsNullOrEmpty(OptionalText) ? 0 : OptionalText.Length));
+
+            for (int i = 0; i <number.Length+1 ; i++)
+            {
+                ToSend.Click(WindowsInput.Events.KeyCode.Backspace);
+            }
+
+            ToSend.Click(number);
+
+            using (Keyboard.Suspend())
+            {
+                ToSend.Invoke();
+            }
+        }
     }
 }
